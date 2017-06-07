a7d4aed4479e0f18fa2d3fef98bf1c726ca0b446
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-07 22:43:38 +0100

    Update train.lua
    Bypass Model proxy

--- train/train.lua
+++ train/train.lua
@@ -26,11 +26,11 @@ function Train:__init(data,model,loss,config)
    self.epoch = config.epoch or 1
 
    -- Get the parameters and gradients
-   self.params, self.grads = self.model:getParameters()
+   self.params, self.grads = self.model.sequential:getParameters()
    self.old_grads = self.grads:clone():zero()
 
    -- Make the loss correct type
-   self.loss:type(self.model:type())
+   self.loss:type(self.model.sequential:type())
 
    -- Find the current rate
    local max_epoch = 1
@@ -57,7 +57,7 @@ function Train:run(epoches,logfunc)
    if self.recapture then
       self.params,self.grads = nil,nil
       collectgarbage()
-      self.params,self.grads = self.model:getParameters()
+      self.params,self.grads = self.model.sequential:getParameters()
       collectgarbage()
    end
    -- The loop
@@ -73,15 +73,15 @@ function Train:batchStep()
    -- Get a batch of data
    self.batch_untyped,self.labels_untyped = self.data:getBatch(self.batch_untyped,self.labels_untyped)
    -- Make the data to correct type
-   self.batch = self.batch or self.batch_untyped:transpose(2, 3):contiguous():type(self.model:type())
-   self.labels = self.labels or self.labels_untyped:type(self.model:type())
+   self.batch = self.batch or self.batch_untyped:transpose(2, 3):contiguous():type(self.model.sequential:type())
+   self.labels = self.labels or self.labels_untyped:type(self.model.sequential:type())
    self.batch:copy(self.batch_untyped:transpose(2, 3):contiguous())
    self.labels:copy(self.labels_untyped)
 
 
    -- Forward propagation
-   self.output = self.model:forward(self.batch)
-   self.objective = self.loss:forward(self.output,self.labels)
+   self.output = self.model.sequential:forward(self.batch)
+   self.objective = self.loss.sequential:forward(self.output,self.labels)
    if type(self.objective) ~= "number" then self.objective = self.objective[1] end
    self.max, self.decision = self.output:double():max(2)
    self.max = self.max:squeeze():double()
@@ -97,7 +97,7 @@ function Train:batchStep()
    -- Backward propagation   
    self.grads:zero()
    self.gradOutput = self.loss:backward(self.output,self.labels)
-   self.gradBatch = self.model:backward(self.batch,self.gradOutput)
+   self.gradBatch = self.model.sequential:backward(self.batch,self.gradOutput)
 
    -- Update the step
    self.old_grads:mul(self.momentum):add(self.grads:mul(-self.rate))
