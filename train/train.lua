0445acead24c58e99e7d2b0307cf2c76bdc1a2ce
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-08 16:41:54 +0100

    Create train.lua

--- train/train.lua
+++ train/train.lua
@@ -4,34 +4,20 @@ local Train = torch.class("Train")
 
 function Train:__init(data, network)
    -- Set vars
+   self.data = data;
    self.module = network:model();
    self.criterion = network:loss();
-   
-   self:formatData(data);
+   self.dataset = self:formatData(data);
 
    -- Load Network into GPU
    self.module = self.module:cuda();
    self.criterion = self.criterion:cuda();
+   self.dataset = self.dataset:cuda()
    
    print("Ready to train...")
 end
 
 function Train:formatData(data)
-   
-   --local randomClass = torch.random(#data.data.index)
-   --^^ Random select of one of the classifications.
-   
-   --local randomDataFromClass = torch.random(data.data.index[randomClass]:size(1))
-   --^^ Random select or one of the data inputs from teh selected Class.
-   
-   --local dataString = ffi.string(
-   --   torch.data(
-   --      data.data.content:narrow(
-   --         1, data.data.index[randomClass][randomDataFromClass][( data.data.index[randomClass][randomDataFromClass]:size(1) )], 1
-   --      )
-   --   )
-   --):lower();
-   --^^  ¯\_(ツ)_/¯ -- No clue what this is doing, but this is the string of the input
    
    local formatedData = {}
    for class = 1, #data.data.index do
@@ -40,13 +26,15 @@ function Train:formatData(data)
       
       for dataID = 1, data.data.index[class]:size(1) do
        
-         local dataString = ffi.string(
-            torch.data(
-               data.data.content:narrow(
-                  1, data.data.index[class][dataID][( data.data.index[class][dataID]:size(1) )], 1
+         local dataTensor = self:toTensor(
+            ffi.string(
+               torch.data(
+                  data.data.content:narrow(
+                     1, data.data.index[class][dataID][( data.data.index[class][dataID]:size(1) )], 1
+                  )
                )
-            )
-         ):lower();
+            ):lower()
+         , 1014);
          
          table.insert(formatedData, {dataString, class});
       end
@@ -54,12 +42,6 @@ function Train:formatData(data)
    
    return formatedData
    
-   
-   --local dataString = 'abcdefghijklmnopqrstuvwxyz'
-   
-   --print(dataString)
-   --print('Data Length: '..#dataString)
-   --print('Alphabet Length:'..#data.alphabet)
    
    --print('---')
    --local tensor = torch.Tensor(#data.alphabet, 1014)
@@ -75,6 +57,20 @@ function Train:formatData(data)
    
 end
 
+
+function Train:toTensor(data, length)
+   
+   local tensor = torch.Tensor(#self.data.alphabet, length);
+   tensor:zero();
+   for i = #data, math.max(#data - length + 1, 1), -1 do
+      
+      if self.data.dict[data:sub(i,i)] then
+         tensor[self.data.dict[data:sub(i,i)]][#data - i + 1] = 1;
+      end
+   end
+   
+   return tensor;
+end
 
 function Train:run()
    
