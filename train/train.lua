5966001bf99a9e75d2ea530ac0d3bda90fac2e0f
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-09 11:09:38 +0100

    Create train.lua

--- train/train.lua
+++ train/train.lua
@@ -29,7 +29,7 @@ function Train:loadData(data, batchSize)
       
       for dataID = 1, data.data.index[class]:size(1) do
        
-         table.insert(formatedData.batch['data'], ffi.string(
+         table.insert(formatedData[batch]['data'], ffi.string(
             torch.data(
                data.data.content:narrow(
                   1, data.data.index[class][dataID][( data.data.index[class][dataID]:size(1) )], 1
@@ -37,7 +37,7 @@ function Train:loadData(data, batchSize)
             )
          ):lower())
          
-         table.insert(formatedData.batch['label'], class);
+         table.insert(formatedData[batch]['label'], class);
          i = i + 1;
          
          if (i % batchSize == 0) then
