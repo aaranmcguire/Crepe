bf8520addc284f57bda444b57c2838c266dcd679
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-09 21:22:22 +0100

    Create train.lua

--- train/train.lua
+++ train/train.lua
@@ -8,7 +8,7 @@ function Train:__init(data, network)
    self.data = data;
    self.alphabet = data.alphabet;
    self.dict = data.dict;
-   self.batchSize = 99;
+   self.batchSize = 100;
    
    self.module = network:model();
    self.criterion = network:loss();
