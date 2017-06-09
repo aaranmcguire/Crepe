e8b28bff2fa0c2ea3be8e2da6fec532da5815c12
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-09 11:31:32 +0100

    Create train.lua

--- train/train.lua
+++ train/train.lua
@@ -13,20 +13,17 @@ function Train:__init(data, network)
    self.module = self.module:cuda();
    self.criterion = self.criterion:cuda();
    
-   self:loadData(data, 1000)
+   salf.data = self:loadData(data)
+   
+   createBatches()
    
    print("Ready to train...")
 end
 
 function Train:loadData(data, batchSize)
-   local i = 1;
-   local batch = 1;
    local formatedData = {}
 
    for class = 1, #data.data.index do
-      print('Class #:'..class);
-      print('# of data in class: '..data.data.index[class]:size(1));
-      
       for dataID = 1, data.data.index[class]:size(1) do  
          
          table.insert(formatedData, {
@@ -40,19 +37,29 @@ function Train:loadData(data, batchSize)
                
             ["label"] = class
          });
-         
-         i = i + 1;
-         
-         --if (i % batchSize == 0) then
-         --   batch = batch + 1
-         --end
-
       end
       collectgarbage()
    end
 
    return formatedData
+end
 
+function Train:createBatches(batchSize)
+   local batch = 1
+   local batchSize = batchSize or 1000
+   local batches = {}
+   
+   for i = 1, #self.data do
+      
+      table.insert(batches.batch, self.data[i])
+               
+      if (i % batchSize == 0) then
+         batch = batch + 1
+         print("Batch: "..batch)
+      end
+   end
+   
+   return batches
 end
 
 function Train:loadBatch(num)
