d9f489d0c7a8759d3b38fa24855ad87eb40efb60
Aaran McGuire <aaran.mcguire@cl.vc>
2017-06-17 19:54:46 +0100

    Update test.lua

--- train/test.lua
+++ train/test.lua
@@ -39,14 +39,21 @@ function Train.main()
            trainset.data = trainset.data:cuda()
            trainset.label = trainset.label:cuda()
            
+	   local correct = 1
            for t = 1,trainset:size() do
 		local prediction = module:forward(trainset.data[t])
 		local confidences, indices = torch.sort(prediction, true)
 		print("Prediction: "..indices[1])
 		print("Fact "..trainset.label[t])
+		
+		if indices[1] == trainset.label[t] then
+			correct = correct + 1
+		end
 			
 	   end
       
+	   print("Accuracy: "..correct.."/"..trainset:size().."("..(100*correct)/trainset:size()..")")
+		
            collectgarbage()
         end
 	
