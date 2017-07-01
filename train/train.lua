4941a08574694822a85353ba52adb62e1dbeb5a4
Aaran McGuire <aaran.mcguire@cl.vc>
2017-07-01 14:19:15 +0100

    Update train.lua

--- train/train.lua
+++ train/train.lua
@@ -21,7 +21,7 @@ function Train:run()
    
    trainer = nn.StochasticGradient(self.module, self.criterion)
    trainer.learningRate = 0.001
-   trainer.maxIteration = 100
+   trainer.maxIteration = 500
    trainer.shuffleIndices = false
    
    print("Number of batches:"..#self.data.batches)
